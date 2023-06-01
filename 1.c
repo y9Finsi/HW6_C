2134971e6fde9ff53dd8b862ab5b7f2bdffd9b6b
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-06-01 23:38:10 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    if (a > b) {  // если a > b, меняем их местами
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    
+    for (int i = a; i <= b; i++) { // перебираем числа от a до b
+        if (i % d == c) {  // проверяем, остаток при делении на d равен c
+            printf("%d ", i); // выводим число, если условие верно
+        }
+    }
+        
+    return 0;
+}
